e233daf8f1b10cb4bc67d4e4998b0ec77f1e1d0c
hanbyeol Chae <91665584+hanbyeol4869@users.noreply.github.com>
2022-11-17 14:12:19 +0900

    Add files via upload
    bash update 2022.11.17 14:12

---
+++ rhead.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 사용법: ./rhead.bash [디렉터리]
+# 대상 디렉터리와 모든 하위 디렉터리 내에 있는 파일들의 헤더를 프린트
+
+cd $1
+for file in *
+do
+    if [ -f $file ]
+    then
+        echo "========= $file ========="
+        head $file
+    fi
+    if [ -d $file ]
+    then
+        /home/h20211519/rhead.bash $file
+    fi
+done
